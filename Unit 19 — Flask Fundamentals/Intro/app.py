9347694e9efadfbb0473b89bef2102967b7ab7ed
Kaitlin Berryman <k.berrygirl.10@gmail.com>
2022-01-03 17:41:23 -0500

    Fix accident

--- Unit 19 — Flask Fundamentals/Intro/app.py
+++ Unit 19 — Flask Fundamentals/Intro/app.py
@@ -1,214 +1,123 @@
-# Flask Jinja
+from flask import Flask, request
 
-Respond to HTTP requests with HTML **seperated/organized in a separate HTML file** .
+app = Flask(__name__)
 
-## Introduction to Templates
-
-### Review 
-- Views are functions that return a string of HTML. So far, we've written all our HTML in-line.
-- Routes are declared by @decorators right before a view function. These also determine that URL which runs a view function.
-
-### What are Templates ... ?
-- Separate, stand-alone files which produce HTML
-- Allows dynamic responses by using variables passed from the views. Can also include loops & conditionals.
-- Repeat the same pattern with different content (like post/comment formats on social media)
-- Inherit from other templates to reduce repetition
-
-### What's Jinja ... ?
-A template system for Python that comes with the Flask install
-
-To use Jinja, create a templates directory. Flask is looking for a directory with that name in the same directory as our main app.py file.
-
-In your Flask directory with a functioning app.py, do the following
- - [ ] mkdir templates
- - [ ] touch hello.html
- - [ ] add some html to hello.html\
- - [ ] in app.py, `from flask import render_template`
+@app.route('/')
+def homepage():
+    html = """
+    <html>
+      <body>
+        <h1>Homepage</h1>
+        <p>Welcome to this simple website!</p>
+        <a href='/hi'>Click Here for Hello Page</a>
+        <a href='/bye'>Click Here for Bye Page</a>
+      </body>
+     </html>
+    """
+    return html
 
 
-To tie the view to the template, use a function called `render_template("fileName.html")`. This will automatically find **fileName.html** in **templates/**
+@app.route('/hi')
+def sayHi():
+    html = """
+    <html>
+      <body>
+        <h1>Hi</h1>
+      </body>
+     </html>
+    """
+    return html
 
-    from flask import Flask, request, render_template
-    
-    app = Flask(__name__)
-        
-    @app.route('/hi')
-        def sayHi():
-            return render_template("hello.html")
+@app.route('/bye')
+def sayBye():
+    html = """
+    <html>
+      <body>
+        <h1>Goodbye</h1>
+      </body>
+     </html>
+    """
+    return html
 
-This is just a static HTML file.
+# Extract data from the url query string
+@app.route('/search')
+def search():
+    term = request.args["term"]
+    sort = request.args["sort"]
+    print("term... ", term)
+    return f"<h1>Search results for... {term} Sorted by... {sort}<h1>"
 
-## Debug Toolbar
-Install it
+# Set up this end point to accept GET requests
+# In terminal, type the following to make an HTTP GET request
+# curl http://127.0.0.1:5000/demo
+@app.route('/demo')
+def getDemo():
+    return "You made a get request"
 
-    (venv) pip3 install flask-debugtoolbar
-    
-Add it as a dependency
-
-   `(venv) pip3 freeze > requirements.txt`
-
-Add it to add.py
-
-    from flask_debugtoolbar import DebugToolbarExtension
-
-After `app = Flask(__name__)`, config a secret key
-
-    app.config['SECRET_KEY'] = "someRandomKey"
-    debug = DebugToolbarExtension(app)
-
-Now, look in your browser!! The debug bar is on the side of every route using a template
-
-The debugging tool shows us...
-* Request Vars
-* HTTP Headers
-* Templates
-* Route List
-
-## Jinja Variables
-Currently, our template is static HTML.
-Let's add variables! Jinja will replace **{{ message }}** with the value of **message** passed in app.py. Anything inside of those curly braces will run as python.
-
-app.py
-
-    from random import randint
-    
-    @app.route('/rand')
-    def randNum():
-        num = randint(1, 100)
-        return render_template('rand.html', random_number=num)
-
-rand.html
-
-    <h1>Random Number - {{random_number}}</h1>
-
-It works! We have a dynamic template
-
-## Greeter Demo
-Receives username through a form & greets the username with a random compliment
-
-app.py
-
-    COMPLIMENTS = ["cool","clever","smart"]
-
-    @app.route('/form')
-    def showForm():
-        return render_template('form.html')
-    
-    # Connect by editing form.html form action="/greet"
-    # greet.html now has access to username and compliment
-    @app.route('/greet')
-    def getGreeting():
-        username = request.args["username"]
-        compliment = choice(COMPLIMENTS)
-        return render_template('greet.html', username=username, compliment=compliment)
-
-form.html
-
-    <body>
-      <form action="/greet">
-        <input type="text" placeholder="Your Name" name="username">
-        <button>Submit</button>
-      </form>
-    </body>
-
-greet.html
-
-    <h1>Greetings, {{username}}! You are {{compliment}}</h1>
-
-## Jinja Conditionals
-How we display different content in the same template depending on something such as if a post has been liked or not.
-
-app.py
-
-    from flask import Flask, request, render_template
-    from flask_debugtoolbar import DebugToolbarExtension
-    from random import randint, choice
-    
-    app = Flask(__name__)
-    
-    app.config['SECRET_KEY'] = "someRandomKey"
-    debug = DebugToolbarExtension(app)
-
-    # Jinja Conditionals Demo
-    # In lucky.html, add {% if condition %} do-xyz {% endif %}
-    @app.route('/lucky')
-    def luckyNum():
-        num = randint(1, 10)
-        return render_template('lucky.html', random_number=num)
+# Set the same endpoint/path to also accept POST requests
+# In terminal, type the following to make an HTTP POST request
+# curl -X POST http://127.0.0.1:5000/demo
+@app.route('/demo', methods=["POST"])
+def postDemo():
+    return "You made a post request"
 
 
-lucky.html
+# Now, let's use a form
+# The name attribute in input stores the value to send it to the server
+@app.route('/add-comment')
+def addCommentForm():
+    return """
+        <h1>Add Comment</h1>
+        <form method="POST">
+          <input type='text' placeholder='Username' name='username'/>
+          <input type='text' placeholder='Password' name='password'/>
+          <button>submit</button>
+        </form>
+    """
 
-    <h1>Random Number - {{random_number}}</h1>
-    
-    {% if random_number == 3 %}
-      <h2>That's your lucky number!</h2>
-    {% else %}
-      <h2>Try again</h2>
-    {% endif %}
+# This is triggered when the above form is submitted
+# To extract form data, use request.form
+@app.route('/add-comment', methods=["POST"])
+def saveComment():
+    username = request.form['username']
+    password = request.form['password']
+    return f"""
+        <h1>Your username -- {username}</h1>
+        <h2>Your password -- {password}</h2
+    """
 
-## Jinja Loops
-For every comment in this post, do this logic.
 
-    {% for VAR in ITERABLE %} ... {% endfor %}
- app.py
+# At the moment, all our paths are hard-coded
+# We don't want all of those end-points.. let's look for patterns in the url
+# Wrapping the query string in <> turns it into a variable
+# It needs to be passed to the function too
+@app.route('/r/<subreddit>')
+def showSubreddit(subreddit):
+    return f"<h1>this is a subreddit for {subreddit}</h1>"
 
-    # Jinja Loops Demo
-    # Spell the word in new h3s
-    @app.route('/spell/<word>')
-    def spell(word):
-        return render_template('spelling.html', word=word)
 
-spelling.html
+POSTS = {
+  1: "I like coffee",
+  2: "Mayo is soft",
+  3: "I don't know what to write"
+}
 
-    <body>
-      <h1>Your word is {{word}}</h1>
-    
-      {% for char in word %}
-        <h3>{{char.upper()}}</h3>
-      {% endfor %}
-    </body>
+# Cast id to int. This forces the url to follow this format.
+# .get here provides a default error handle
+@app.route('/posts/<int:id>')
+def showPosts(id):
+    post = POSTS.get(id, "Error -- no post here");
+    return f"{post}"
 
-## Greeter Demo v2
-app.py
 
-    # Jinja Greeter Demo v2
-    @app.route('/form-2')
-    def showForm2():
-        return render_template('form2.html')
-    
-    @app.route('/greet-2')
-    def getGreeting2():
-        username = request.args["username"]
-    
-        # Use .get - if it's there, use it. if not, no errors please
-        wants_compliments = request.args.get("wants_compliments")
-        compliments = sample(COMPLIMENTS, 3)
-        return render_template("greet2.html", username=username, wants_compliments=wants_compliments, compliments=compliments)
+# Let's try multiple variables
+@app.route('/r/<subreddit>/comments/<int:postid>')
+def showCommends(subreddit, postid):
+    return f"<h1>Viewing comment {postid} from {subreddit}</h1>"
 
-form2.html
-
-    <body>
-      <form action="/greet-2">
-        <input type="text" placeholder="Your Name" name="username">
-    
-        <label for="compliments">Do you want compliments?</label>
-        <input type="checkbox" id="compliments" name="wants_compliments">
-        <button>Submit</button>
-      </form>
-    </body>
-
-greet2.html
-
-    <body>
-      <h1>Hi there, {{username}}!</h1>
-    
-      {% if wants_compliments %}
-        <h3>Here's your compliments!</h3>
-        <ul>
-          {% for compliment in compliments %}
-            <li>You are {{compliment}}</li>
-          {% endfor %}
-        </ul>
-      {% endif %}
-    </body>
+# Distinction between route variables and query strings
+# Both pass in information to a get requests
+# URL Param/route variable is like the subject of the page .. feels required
+# URL Param/route variables aka /shop/<toy>
+# Query Params aka /shop?toy=spinneer
+# Query Params feels like extra info. Info often comes from a form...?
