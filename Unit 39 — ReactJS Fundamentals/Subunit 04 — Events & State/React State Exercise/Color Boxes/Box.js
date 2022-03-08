db515bbaa72937d3905e7150a5cdef8544151a10
Kaitlin Berryman <k.berrygirl.10@gmail.com>
2022-03-08 16:29:22 -0500

    Create Box.js

---
+++ Unit 39 — ReactJS Fundamentals/Subunit 04 — Events & State/React State Exercise/Color Boxes/Box.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Box.css";
+
+function Box({ color }) {
+  return (
+    <div style={{backgroundColor: color}}>
+      ______
+    </div>
+  );
+}
+
+export default Box;
